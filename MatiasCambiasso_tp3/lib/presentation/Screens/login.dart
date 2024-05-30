a093e62ffde910d93140326700557adae53c2edf
cammbi <matiascambiasso@gmail.com>
2024-05-29 21:55:11 -0300

    listas

--- MatiasCambiasso_tp3/lib/presentation/Screens/login.dart
+++ MatiasCambiasso_tp3/lib/presentation/Screens/login.dart
@@ -46,18 +46,39 @@ class LoginScreen extends StatelessWidget {
                 String inputUser = userController.text;
                 String inputPass = passController.text;
 
-                if(inputPass.isEmpty || inputUser.isEmpty){
-                  print("Contraseña o User vacíos");
-                }
-
-                if ((inputUser == "Cambi") && (inputPass == "2007") ){
-                  print("Inicio de sesión exitoso");
-                  
-                  context.pushNamed(HomeScreen.name , extra: inputUser);
-                }
-                else{
-                  print("Inicio de sesión fallido");
-                }
+                var indexUser = users.indexOf(inputUser);
+                  var indexPass = passwords.indexOf(inputPass);
+                  if(indexUser == indexPass && indexUser != -1){
+                    context.pushNamed(HomeScreen.name , extra: inputUser);
+                  }
+                  else if(userController.text == '' && passController.text == ''){
+                    SnackBar snackBar = const SnackBar(
+                      content: Text("Usuario y Contraseña Vacios"),
+                      duration: Duration(seconds: 3),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  else if(userController.text == ''){
+                    SnackBar snackBar = const SnackBar(
+                      content: Text("Usuario Vacio"),
+                      duration: Duration(seconds: 3),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  else if(passController.text == ''){
+                    SnackBar snackBar = const SnackBar(
+                      content: Text("Contraseña Vacia"),
+                      duration: Duration(seconds: 3),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  else{
+                    SnackBar snackBar = const SnackBar(
+                      content: Text("Usuario o Contraseña Incorrecto"),
+                      duration: Duration(seconds: 3),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
                 }, 
               
               child: const Text('Login'), 
--- MatiasCambiasso_tp3/lib/presentation/Screens/welcome.dart
+++ MatiasCambiasso_tp3/lib/presentation/Screens/welcome.dart
@@ -5,6 +5,9 @@ import 'package:go_router/go_router.dart';
 class HomeScreen extends StatelessWidget {
 static const String name = 'home';
 
+List<String> titulos = ["Milanesa de pollo", "Fideos con tuco", "Hamburguesas", "Panchos", "Papas fritas", "Tequeños", "Pollo al curry", "Ñoquis", "Tarta", "Empanada", "Pizza", "Burrito"];
+List<String> descripcion = ["Pechuga rebozada en pan rayado", "Tallarines con salsa de tomate", "Hamburguesa con cheddar y bacon ", "Panchos con mayo y mostaza", "Papas fritas para acompañar", "Bastoncitos de queso enrrollados", "Pollo con mezcla de especias", "Ñoquis caseros con bolognesa", "Tartita de JyQ", "Empanada de carne cortada a cuchillo", "Pizza de panceta y verdeo", "Burrito bien cargado" ];
+
 String userName;
  HomeScreen({super.key , this.userName = ""});
   
@@ -12,21 +15,18 @@ String userName;
   @override
   Widget build(BuildContext context) {
 
-
     return  Scaffold(
       appBar: AppBar (
         
-        title: const Icon(Icons.home),
+        title: const Text("Comidas"),
         ),
-      body:  Center(
-        child: Row (mainAxisAlignment: MainAxisAlignment.center,
-            
-            children: [
-              const Text('Bienvenido ', style:  TextStyle( fontSize: 30),),
-               Text(userName , style: const TextStyle(fontSize: 30),),
-        
-          ],),
-      ),
+      body:  ListView.builder(itemCount: titulos.length, itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(titulos[index],),
+                    subtitle: Text(descripcion[index]),)
+                );
+              },)
     );
   }
 }
