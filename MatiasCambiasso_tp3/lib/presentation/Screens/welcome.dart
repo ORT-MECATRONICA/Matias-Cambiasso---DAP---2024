3dddc6ac5ea5a3826c1178fb64f286a8bfb15e29
Uridyner <tortillas1152@gmail.com>
2024-05-30 14:22:14 -0300

    .

--- MatiasCambiasso_tp3/lib/presentation/Screens/welcome.dart
+++ MatiasCambiasso_tp3/lib/presentation/Screens/welcome.dart
@@ -5,8 +5,40 @@ import 'package:go_router/go_router.dart';
 class HomeScreen extends StatelessWidget {
 static const String name = 'home';
 
-List<String> titulos = ["Milanesa de pollo", "Fideos con tuco", "Hamburguesas", "Panchos", "Papas fritas", "Tequeños", "Pollo al curry", "Ñoquis", "Tarta", "Empanada", "Pizza", "Burrito"];
-List<String> descripcion = ["Pechuga rebozada en pan rayado", "Tallarines con salsa de tomate", "Hamburguesa con cheddar y bacon ", "Panchos con mayo y mostaza", "Papas fritas para acompañar", "Bastoncitos de queso enrrollados", "Pollo con mezcla de especias", "Ñoquis caseros con bolognesa", "Tartita de JyQ", "Empanada de carne cortada a cuchillo", "Pizza de panceta y verdeo", "Burrito bien cargado" ];
+List<String> titulos = [
+  "Milanesa de pollo",
+  "Fideos con tuco", 
+  "Hamburguesas",
+  "Panchos",
+  "Papas fritas",
+  "Tequeños", 
+  "Pollo al curry",
+  "Ñoquis",
+  "Tarta",
+  "Empanada",
+  "Pizza",
+  "Burrito"
+ ];
+List<String> descripcion = [
+    "Pechuga rebozada en pan rayado",
+    "Tallarines con salsa de tomate",
+    "Hamburguesa con cheddar y bacon",
+    "Panchos con mayo y mostaza",
+    "Papas fritas para acompañar",
+    "Bastoncitos de queso enrollados",
+    "Pollo con mezcla de especias",
+    "Ñoquis caseros con bolognesa",
+    "Tartita de JyQ",
+    "Empanada de carne cortada a cuchillo",
+    "Pizza de panceta y verdeo",
+    "Burrito bien cargado"
+  ];
+
+List<String> imagenes = [
+    "C:\Users\android\Documents\GitHub\Matias-Cambiasso---DAP---2024\MatiasCambiasso_tp3\assets\milanesa.jpg",
+    "C:\Users\android\Documents\GitHub\Matias-Cambiasso---DAP---2024\MatiasCambiasso_tp3\assets\fideos.jpg",
+    // Agrega las rutas de las imágenes restantes aquí
+  ];
 
 String userName;
  HomeScreen({super.key , this.userName = ""});
@@ -23,6 +55,11 @@ String userName;
       body:  ListView.builder(itemCount: titulos.length, itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
+                    leading: Image.asset(
+                imagenes[index], // Aquí se carga la imagen correspondiente
+                width: 50, // Ajusta el ancho según sea necesario
+                height: 50, // Ajusta la altura según sea necesario
+              ),
                     title: Text(titulos[index],),
                     subtitle: Text(descripcion[index]),)
                 );
